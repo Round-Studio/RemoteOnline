174275a283b181b0af85f049b04d173488c95ff2
Dime <liy_0924@qq.com>
2025-10-19 16:38:32 +0800

    feat: 写完了scf服务器，但是我不理解为什么无法打通 scf 的端口转发

--- RemoteOnline.Console/Program.cs
+++ RemoteOnline.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using RemoteOnline.Enum;
 using RemoteOnline.ScaffoldingMc;
 
@@ -6,9 +7,15 @@ class Program
 {
     static async Task Main()
     {
+        Process.GetProcessesByName("easytier-core.exe").ToList().ForEach(x => x.Kill(true));
+        
         RoomCore core = new RoomCore(ServiceStatus.Client);
         core.EasyTierPath = "easytier-core.exe";
         core.EasyTierCliPath = "easytier-cli.exe";
-        await core.CreateRoom(25565);
+        core.HostPlayerName = "Dime";
+        core.HostMachineId = "aaa";
+        // await core.CreateRoom(25565);
+        
+        await core.ConnectRoom("U/TXVN-65UU-EEGH-HGBV");
     }
 }
